5bd0ab0cf3e27951a6affe56064b7e893507bc3e
Billy Derringer <billyderringer7@gmail.com>
2018-07-14 18:35:55 -0500

    started adding test logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 let state = {
     stage: "operation",
     operation: "",
-    difficulty: ""
+    difficulty: "",
+    correct: "",
+    num1: "",
+    num2: "",
+    testQuestions: []
 }
 
 const main = document.getElementById('container-main')
@@ -16,28 +20,28 @@ const operation = `
             </p>
             <div id="container-operations">
                 <a href="#"
-                   onclick="setOperationState('plus')"
+                   onclick="setOperationState('+')"
                    class="operation center-all grow">
                     <div>
                         <i class="fas fa-plus"></i>
                     </div>
                 </a>
                 <a href="#"
-                   onclick="setOperationState('minus')"
+                   onclick="setOperationState('-')"
                    class="operation center-all grow">
                     <div>
                         <i class="fas fa-minus"></i>
                     </div>
                 </a>
                 <a href="#"
-                   onclick="setOperationState('times')"
+                   onclick="setOperationState('*')"
                    class="operation center-all grow">
                     <div>
                         <i class="fas fa-times"></i>
                     </div>
                 </a>
                 <a href="#"
-                   onclick="setOperationState('divide')"
+                   onclick="setOperationState('/')"
                    class="operation center-all grow">
                     <div>
                         <i class="fas fa-divide"></i>
@@ -100,18 +104,18 @@ function runStageCheck(){
 }
 
 function setOperationState (operation) {
-    if(operation === 'plus'){
-        state.operation = 'plus'
-        sessionStorage.setItem('operation', 'plus')
-    }else if(operation === 'minus'){
-        state.operation = 'minus'
-        sessionStorage.setItem('operation', 'minus')
-    }else if(operation === 'times'){
-        state.operation = 'times'
-        sessionStorage.setItem('operation', 'times')
-    }else if(operation === 'divide'){
-        state.operation = 'divide'
-        sessionStorage.setItem('operation', 'divide')
+    if(operation === '+'){
+        state.operation = '+'
+        sessionStorage.setItem('operation', '+')
+    }else if(operation === '-'){
+        state.operation = '-'
+        sessionStorage.setItem('operation', '-')
+    }else if(operation === '*'){
+        state.operation = '*'
+        sessionStorage.setItem('operation', '*')
+    }else if(operation === '/'){
+        state.operation = '/'
+        sessionStorage.setItem('operation', '/')
     }else if(operation === 'all-operations'){
         state.operation = 'all-operations'
         sessionStorage.setItem('operation', 'all-operations')
@@ -132,7 +136,35 @@ function setDifficultyState (difficulty) {
         sessionStorage.setItem('difficulty', 'advanced')
     }
     state.stage = 'run-test'
+    createTestArray()
     runStageCheck()
+    console.log(state.testQuestions)
+}
+
+function getRandomNum(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+}
+
+function createQuestion() {
+    if(state.difficulty === 'beginner'){
+        sessionStorage.setItem('num1', getRandomNum(1, 10))
+        sessionStorage.setItem('num2', getRandomNum(1, 10))
+    }else if(state.difficulty === 'intermediate'){
+        sessionStorage.setItem('num1', getRandomNum(10, 100))
+        sessionStorage.setItem('num2', getRandomNum(1, 10))
+    }else if(state.difficulty === 'advanced'){
+        sessionStorage.setItem('num1', getRandomNum(10, 100))
+        sessionStorage.setItem('num2', getRandomNum(10, 100))
+    }
+}
+
+function createTestArray() {
+   for(let i=0;i<10;i++){
+       createQuestion()
+    state.testQuestions.push([sessionStorage.getItem('num1'),sessionStorage.getItem('num2')])
+   }
 }
 
 runStageCheck()
